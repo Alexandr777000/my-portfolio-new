10ad0bab2a3eec81ee443d3ba3f6c4cf18e4e3c0
Aleksandr_UB <bogdanov777000@mail.ru>
2023-04-23 16:33:08 +0300

    made the logic and styling of the burger part_1

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,14 @@
 import {Link} from "react-scroll";
 import styles from "@/styles/header.module.scss";
 import Logo from "@/components/Logo/Logo";
+import {useMediaQuery} from "@/hooks";
+import {useState} from "react";
 
 const Header = () => {
+    const isMobile = useMediaQuery(665);
+    // открыто меню?
+    const [menuOpen, setMenuOpen] = useState(false);
+
     // при перезагрузке стр. она останется на этой секции
     const spy = true;
     const smooth = true;
@@ -10,17 +16,37 @@ const Header = () => {
     const duration = 500;
     // const currentMenuItemClass =
 
+    const handleToggleMenu = () => {
+        (document.querySelector('body') as HTMLBodyElement).classList.toggle('overflow-hidden');
+        setMenuOpen(!menuOpen);
+    }
+
+    const closeMenu = () => {
+        (document.querySelector('body') as HTMLBodyElement).classList.remove('overflow-hidden');
+        setMenuOpen(false);
+    }
+
     return (
         <header className={styles.header}>
             <div className={`container ${styles.header__container}`}>
                 <Logo/>
+                {/*условный рендеринг бургер-меню*/}
+                {isMobile &&
+                    <button
+                        className={`${styles.burger_menu} ${menuOpen ? styles.open : ""}`}
+                        onClick={handleToggleMenu}
+                    >
+                        <span/>
+                        <span/>
+                        <span/>
+                    </button>}
                 <nav className={styles.header__nav}>
                     <ul className={styles.header__nav__list}>
                         <li className={styles.header__nav__list__item}>
                             <Link
                                 className={styles.header__nav__list__item__link}
                                 href="/"
-                                to="about0"
+                                to="about"
                                 spy={spy}
                                 smooth={smooth}
                                 offset={offset}
