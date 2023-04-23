6a09723961aa6bcd81c38d6a8456b949302c0170
Aleksandr_UB <bogdanov777000@mail.ru>
2023-04-23 16:52:25 +0300

    refactor Header.tsx

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,104 +1,104 @@
 import {Link} from "react-scroll";
-import styles from "@/styles/header.module.scss";
 import Logo from "@/components/Logo/Logo";
 import {useMediaQuery} from "@/hooks";
 import {useState} from "react";
 
-const Header = () => {
-    const isMobile = useMediaQuery(665);
-    // открыто меню?
-    const [menuOpen, setMenuOpen] = useState(false);
+import styles from "@/styles/header.module.scss";
+import stylesMenu from "@/styles/mobileMenu.module.scss";
 
-    // при перезагрузке стр. она останется на этой секции
+
+const Header = () => {
+    const isMobile = useMediaQuery(640);
+    const [menuOpen, setMenuOpen] = useState(false);
     const spy = true;
     const smooth = true;
     const offset = 140;
     const duration = 500;
-    // const currentMenuItemClass =
+    const currentMenuItemClass = isMobile ? stylesMenu.menu__item : styles.header__nav__list__item;
 
     const handleToggleMenu = () => {
-        (document.querySelector('body') as HTMLBodyElement).classList.toggle('overflow-hidden');
+        (document.querySelector("body") as HTMLBodyElement).classList.toggle("overflow-hidden");
         setMenuOpen(!menuOpen);
-    }
+    };
 
     const closeMenu = () => {
-        (document.querySelector('body') as HTMLBodyElement).classList.remove('overflow-hidden');
+        (document.querySelector("body") as HTMLBodyElement).classList.remove("overflow-hidden");
         setMenuOpen(false);
-    }
+    };
 
     return (
         <header className={styles.header}>
             <div className={`container ${styles.header__container}`}>
                 <Logo/>
-                {/*условный рендеринг бургер-меню*/}
-                {isMobile &&
-                    <button
-                        className={`${styles.burger_menu} ${menuOpen ? styles.open : ""}`}
-                        onClick={handleToggleMenu}
-                    >
-                        <span/>
-                        <span/>
-                        <span/>
-                    </button>}
-                <nav className={styles.header__nav}>
-                    <ul className={styles.header__nav__list}>
-                        <li className={styles.header__nav__list__item}>
+                {isMobile && <button onClick={handleToggleMenu}
+                                     className={`${styles.burger_menu} ${menuOpen ? styles.open : ""}`}>
+                    <span/>
+                    <span/>
+                    <span/>
+                </button>}
+                <nav
+                    className={`${isMobile ? stylesMenu.menu : styles.header__nav} ${menuOpen ? stylesMenu.open : ""}`}>
+                    <ul className={`${isMobile ? styles.list_reset : styles.header__nav__list}`}>
+                        <li className={currentMenuItemClass}>
                             <Link
-                                className={styles.header__nav__list__item__link}
                                 href="/"
                                 to="about"
                                 spy={spy}
                                 smooth={smooth}
                                 offset={offset}
                                 duration={duration}
+                                className={styles.header__nav__list__item__link}
+                                onClick={closeMenu}
                             >
                                 Обо мне
                             </Link>
                         </li>
-                        <li className={styles.header__nav__list__item}>
+                        <li className={currentMenuItemClass}>
                             <Link
-                                className={styles.header__nav__list__item__link}
                                 href="/"
-                                to="about0"
+                                to="skills"
                                 spy={spy}
                                 smooth={smooth}
                                 offset={offset}
                                 duration={duration}
+                                className={styles.header__nav__list__item__link}
+                                onClick={closeMenu}
                             >
                                 Навыки
                             </Link>
                         </li>
-                        <li className={styles.header__nav__list__item}>
+                        <li className={currentMenuItemClass}>
                             <Link
-                                className={styles.header__nav__list__item__link}
                                 href="/"
-                                to="about0"
+                                to="portfolio"
                                 spy={spy}
                                 smooth={smooth}
                                 offset={offset}
                                 duration={duration}
+                                className={styles.header__nav__list__item__link}
+                                onClick={closeMenu}
                             >
                                 Портфолио
                             </Link>
                         </li>
-                        <li className={styles.header__nav__list__item}>
+                        <li className={currentMenuItemClass}>
                             <Link
-                                className={styles.header__nav__list__item__link}
                                 href="/"
-                                to="about0"
+                                to="contact"
                                 spy={spy}
                                 smooth={smooth}
                                 offset={offset}
                                 duration={duration}
+                                className={styles.header__nav__list__item__link}
+                                onClick={closeMenu}
                             >
-                                Связаться со мной
+                                Обратная связь
                             </Link>
                         </li>
                     </ul>
                 </nav>
             </div>
         </header>
-
     );
 };
 
