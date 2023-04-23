66754244e2775c263fc012bd227b4e46e60b9cbe
Aleksandr_UB <bogdanov777000@mail.ru>
2023-04-23 18:06:57 +0300

    made backlight title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "@/components/Header/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
     return (
@@ -14,7 +15,7 @@ export default function Home() {
 
             <Header/>
             <main>
-
+                <Hero/>
             </main>
             <div className="background-animation">
                 <div id="ball-one"></div>
